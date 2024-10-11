4e71c35137fc1baad404a499183af7aa4017fa0e
Rozenberg-Alexsander <rozenberg.s@mail.ru>
2024-10-11 16:49:29 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[20];
+        int[] arr = new int[10];
 
         for (int i = arr[1]; i < arr.length; i++)
             arr[i] = i + 1;
