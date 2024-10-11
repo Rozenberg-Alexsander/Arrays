ba5d9385609cc076e838adf7f5ac3d1f814b6fae
Rozenberg-Alexsander <rozenberg.s@mail.ru>
2024-10-11 16:47:11 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+
+        for (int i = arr[1]; i < arr.length; i++)
+            arr[i] = i + 1;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
